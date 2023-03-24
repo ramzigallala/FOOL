ceecf8bcb8614b628f78e544a22dea91e125e4f5
Ramzi Gallala <ramzi.gallala@studio.unibo.it>
2023-03-24 12:43:30 +0100

    Base for the project

---
+++ gen/svm/SVMListener.java
@@ -0,0 +1,33 @@
+// Generated from java-escape by ANTLR 4.11.1
+package svm;
+
+import java.util.*;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link SVMParser}.
+ */
+public interface SVMListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 */
+	void enterAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SVMParser#assembly}.
+	 * @param ctx the parse tree
+	 */
+	void exitAssembly(SVMParser.AssemblyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void enterInstruction(SVMParser.InstructionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link SVMParser#instruction}.
+	 * @param ctx the parse tree
+	 */
+	void exitInstruction(SVMParser.InstructionContext ctx);
+}
